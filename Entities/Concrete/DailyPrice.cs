ffb6ecbc96e8e9d03a442845f5c1885e66181a4a
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-09-16 16:05:13 +0300

    Katmanlar oluşturuldu. InMemory'de crud işlemleri yapıldı.

---
+++ Entities/Concrete/DailyPrice.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class DailyPrice : IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
